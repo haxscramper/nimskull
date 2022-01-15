6ffdd2c241966b4f02ae32ff39dccd918279186f
haxscramper <haxscramper@gmail.com>
2022-01-15 22:49:09 +0300

    [CHECKPOINT]

--- tests/osproc/treadlines.nim
+++ tests/osproc/treadlines.nim
@@ -6,18 +6,20 @@ Error: cannot open 'b.nim'
   targets: "c"
 """
 
-import osproc
+import std/[osproc, strutils]
 from std/os import getCurrentCompilerExe
 
 var ps: seq[Process] # compile & run 2 progs in parallel
 const nim = getCurrentCompilerExe()
 for prog in ["a", "b"]:
-  ps.add startProcess(nim, "",
-                      ["r", "--hint:Conf:off", "--hint:Processing:off", prog],
-                      options = {poUsePath, poDaemon, poStdErrToStdOut})
+  ps.add startProcess(
+    nim, "", ["r", "--hint:Conf:off", "--hint:Processing:off", prog],
+    options = {poUsePath, poDaemon, poStdErrToStdOut})
 
 for p in ps:
   let (lines, exCode) = p.readLines
   if exCode != 0:
-    for line in lines: echo line
+    for line in lines:
+      echo line.split("\n")[0]
+
   p.close
